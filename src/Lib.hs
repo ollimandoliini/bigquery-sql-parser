f9f28350d3b8f0d6aef523dfb02db9036274f520
Olli Kortelahti <okortelahti@gmail.com>
2022-01-28 16:38:13 +0200

    Add initial support for WHERE, GROUP BY and HAVING

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,10 @@
-{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings     #-}
+{-# LANGUAGE QuasiQuotes           #-}
 module Lib where
 
-import           Control.Monad              (void)
+import           Control.Monad              (guard, void)
+import           Data.Functor               (($>))
 import qualified Data.Set                   as Set
 import           Data.Text                  (Text)
 import           Data.Text.IO               (readFile)
@@ -10,7 +13,8 @@ import           Prelude                    hiding (readFile)
 import           Text.Megaparsec
 import           Text.Megaparsec.Char
 import qualified Text.Megaparsec.Char.Lexer as L
-import Data.Functor (($>))
+import           Text.RawString.QQ
+
 
 
 type Parser = Parsec Void String
@@ -31,53 +35,114 @@ symbol = L.symbol sc
 symbol' :: String -> Parser String
 symbol' = L.symbol' sc
 
-data Column
+data Query = Query {
+    selectColumns   :: [SelectColumn]
+    , fromTable     :: FromStatement
+    , whereClause   :: Maybe WhereClause
+    , groupByClause :: Maybe GroupByClause
+    , havingClause  :: Maybe HavingClause
+ } deriving Show
+
+data SelectColumn
     = Star
-    | ColumnName {
-    name :: String
-    , alias :: Maybe String
-} deriving Show
+    | PlainColumnName { name :: String, alias :: Maybe String}
+    | FunctionApplication { functionName :: String, argument :: [String], alias :: Maybe String}
+    deriving Show
+
+newtype FromStatement = FromStatement String deriving Show
+
+newtype WhereClause = WhereClause { stmnt :: String } deriving Show
+
+newtype GroupByClause = GroupByClause [String] deriving Show
+
+newtype HavingClause = HavingClause { stmnt :: String } deriving Show
 
 
-newtype TableName = TableName String deriving Show
-
-data Select = Select [Column] TableName deriving Show
 
 reservedKeyWords :: Set.Set String
 reservedKeyWords = Set.fromList ["FROM"]
 
+pStar = symbol "*" $> Star
 
-pColumn :: Parser Column
-pColumn =
-    symbol "*" $> Star
-    <|> pColumnName
+
 
 pName :: Parser String
 pName = do
     name <- some letterChar
-    if name `Set.member` reservedKeyWords
-        then
-            fail $ "Reserved keyword: " <> name
-        else
-            return name
+    guard (not $ name `Set.member` reservedKeyWords)
+    return name
 
 
-pColumnName :: Parser Column
-pColumnName = try $ do
-    colName <- lexeme pName
-    alias <- optional $ symbol' "as" >> lexeme pName
-    return $ ColumnName colName alias
+pColumnName :: Parser SelectColumn
+pColumnName = try $ PlainColumnName <$> lexeme pName <*> optional (symbol' "as" >> lexeme pName)
 
-pTableName :: Parser TableName
-pTableName = TableName <$> lexeme (some letterChar)
-    
+pFunction :: Parser SelectColumn
+pFunction = try $ do
+    functionName <- some letterChar
+    argument <- lexeme $ between "(" ")" (some letterChar `sepEndBy1` symbol ",")
+    alias <- optional (symbol' "as" >> lexeme pName)
+    pure $ FunctionApplication functionName argument alias
 
-pBigQuerySQL :: Parser Select
-pBigQuerySQL = do
-    sc
+pColumn :: Parser SelectColumn
+pColumn
+    = pStar
+    <|> pFunction
+    <|> pColumnName
+
+pSelectList :: Parser [SelectColumn]
+pSelectList = do
     symbol' "SELECT"
-    columns <- pColumn `sepEndBy1` symbol ","
-    symbol' "FROM"    
-    Select columns <$> pTableName
+    pColumn `sepEndBy1` symbol ","
 
-f = parseTest pBigQuerySQL "SELECT *, aa, bb FROM morjesta"
+pFromClause :: Parser FromStatement
+pFromClause = do
+    symbol' "FROM"
+    FromStatement <$> lexeme (some letterChar)
+
+pWhereClause :: Parser (Maybe WhereClause)
+pWhereClause = optional $ do
+    symbol' "WHERE"
+    WhereClause <$> lexeme (some printChar)
+
+pGroupByClause :: Parser (Maybe GroupByClause)
+pGroupByClause = optional $ do
+    symbol' "GROUP BY"
+    foo <- GroupByClause <$> (some letterChar `sepEndBy1` symbol ",")
+    sc
+    return foo
+
+pHavingClause :: Parser (Maybe HavingClause)
+pHavingClause = optional $ do
+    symbol' "HAVING"
+    HavingClause <$> lexeme (some printChar)
+
+
+
+pQuery :: Parser Query
+pQuery = sc >>
+    Query
+        <$> pSelectList
+        <*> pFromClause
+        <*> pWhereClause
+        <*> pGroupByClause
+        <*> pHavingClause
+
+
+sql :: String
+sql = [r|
+SELECT
+    *,
+    aa as yeah,
+    sum(moro) as yeahyeah,
+FROM
+    morjesta
+WHERE
+    b > 1
+GROUP BY
+    a, b
+HAVING
+    y
+|]
+
+f :: IO ()
+f = parseTest pQuery sql
